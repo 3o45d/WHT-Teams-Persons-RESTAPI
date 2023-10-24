8d6186de01970c30af35768d0c014ed74897c00d
Zakhar Lakotsenin <31545d@gmail.com>
2023-10-24 15:28:12 +0300

    Add validation to Person and Team serializers

--- teams/models.py
+++ teams/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=50, verbose_name='Ім\'я')
-    last_name = models.CharField(max_length=50, verbose_name='Прізвище')
-    email = models.EmailField(unique=True, verbose_name='Електронна пошта')
+    first_name = models.CharField(max_length=50, verbose_name='First Name')
+    last_name = models.CharField(max_length=50, verbose_name='Last Name')
+    email = models.EmailField(unique=True, verbose_name='Email')
 
     def __str__(self):
         return self.full_name
@@ -15,9 +15,9 @@ class Person(models.Model):
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Назва команди')
-    description = models.TextField(blank=True, verbose_name='Опис команди')
-    members = models.ManyToManyField(Person, blank=True, related_name='teams')
+    name = models.CharField(max_length=100, unique=True, verbose_name='Name')
+    description = models.TextField(blank=True, verbose_name='Description')
+    members = models.ManyToManyField(Person, blank=True, related_name='teams', verbose_name='Members')
 
     def __str__(self):
         return self.name
